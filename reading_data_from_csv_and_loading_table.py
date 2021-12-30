d5c4c58696797be2dc0507bbadf6c32dc1288e5a
James Alhassan <91744370+abu-naail1989@users.noreply.github.com>
2021-12-30 03:06:30 +0000

    Add files via upload
    Initial Commit

---
+++ reading_data_from_csv_and_loading_table.py
@@ -0,0 +1,23 @@
+# Import insert
+from sqlalchemy import insert
+
+# Create an empty list: values_list
+values_list = []
+
+# Iterate over the rows
+for row in csv_reader:
+    # Create a dictionary with the values
+    data = {'state': row[0], 'sex': row[1], 'age':row[2], 'pop2000': row[3],
+            'pop2008': row[4]}
+    # Append the dictionary to the values list
+    values_list.append(data)
+
+
+# Build insert statement: stmt
+stmt = insert(census)
+
+# Use values_list to insert data: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
